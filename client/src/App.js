c264e559c6c61c4af604798f05d5c99cf59b0cc7
rjo6615 <robertjobernier@gmail.com>
2023-07-24 14:49:42 -0400

    added zombies game mode page

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router,  Route,  Routes,  Navigate} from "react-router
 import Home from "./components/Home/Home";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
+import Zombies from "./components/Zombies/Zombies";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import "./App.css";
@@ -14,6 +15,7 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/zombies" element={<Zombies />} />
           <Route path="*" element={<Navigate to="/"/>} />
         </Routes>
         <Footer />
---
+++ client/src/components/Zombies/Zombies.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Button, Col, Container, Form, Row } from "react-bootstrap";
+
+export default function ZombiesHome() {
+ // This following section will display the form that takes the input from the user.
+ return (
+<body style={{ backgroundImage: 'url(./images/zombie.jpg)', backgroundSize: "cover", backgroundRepeat: "no-repeat"}}>
+<center>
+      <h1 className="text-light">Zombies</h1>    
+      <Container className="mt-5">
+      <Row>
+        <Col sm={12}>
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search Character"
+              className="me-2 rounded-pill"
+              aria-label="Search"
+            />
+            <Button className="rounded-pill" variant="outline-primary">
+              Search
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+    <br></br>
+    <Button href="/Zombie-Random-Creation" className="p-1 m-1" size="lg"  style={{minWidth: 300}} variant="secondary">Create Character (Random)</Button>
+    <Button href="/Zombie-Manual-Creation" className="p-1 m-1" size="lg"  style={{minWidth: 300}} variant="secondary">Create Character (Manual)</Button>
+    <br></br>
+    <br></br>
+    </center>
+    </body>
+ );
+}
