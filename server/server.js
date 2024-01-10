ebc9d8fadfe3a2bd82157297040b665f4709ef8e
rjo6615 <robertjobernier@gmail.com>
2024-01-10 15:05:40 -0500

    added capacitor for android app

--- server/server.js
+++ server/server.js
@@ -3,26 +3,27 @@ const app = express();
 const cors = require("cors");
 require("dotenv").config({ path: "./config.env" });
 const port = process.env.PORT || 5000;
+const path = require('path');
+
 app.use(cors());
 app.use(express.json());
 app.use(require("./routes/routes"));
-const path = require('path')
 
-app.use(express.static('client/build'));
-
+// Adjusted to serve static files from the correct build directory
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 app.get('*', (req, res) => {
+  // Adjusted path for sending index.html
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
 // get driver connection
 const dbo = require("./db/conn");
- 
-app.listen(port, () => {
-  // perform a database connection when server starts
+
+app.listen(port, '0.0.0.0', () => {
+  // perform a database connection when the server starts
   dbo.connectToServer(function (err) {
     if (err) console.error(err);
- 
   });
   console.log(`Server is running on port: ${port}`);
 });
