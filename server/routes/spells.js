d52bfcf88d056cece9d9d641625dc3ce5393e066
Robert Obernier <robertjobernier@gmail.com>
2025-09-03 21:45:23 -0400

    Parse spell damage from descriptions

--- server/routes/spells.js
+++ server/routes/spells.js
@@ -1,6 +1,20 @@
 const express = require('express');
 const spells = require('../data/spells');
 
+// Extract a basic damage dice string (e.g., "8d6" or "1d8+2") from spell descriptions
+function extractDamage(description = '') {
+  const match = description.match(/(\d+d\d+(?:\s*[+\-]\s*\d+)?)[^\n]*damage/i);
+  return match ? match[1].replace(/\s+/g, '') : undefined;
+}
+
+// Augment spells with a `damage` field when possible
+Object.values(spells).forEach((spell) => {
+  if (!spell.damage) {
+    const dmg = extractDamage(spell.description);
+    if (dmg) spell.damage = dmg;
+  }
+});
+
 module.exports = (router) => {
   const spellRouter = express.Router();
 
