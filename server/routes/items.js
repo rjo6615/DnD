923d70e2f55a7440a417a67349afeadc098ce125
Robert Obernier <robertjobernier@gmail.com>
2025-09-07 18:59:11 -0400

    feat: add SRD items dataset and routes

---
+++ server/routes/items.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const { items, types, categories } = require('../data/items');
+
+module.exports = (router) => {
+  const itemRouter = express.Router();
+
+  itemRouter.get('/', (_req, res) => {
+    res.json(items);
+  });
+
+  itemRouter.get('/options', (_req, res) => {
+    res.json({ types, categories });
+  });
+
+  itemRouter.get('/:name', (req, res) => {
+    const item = items[req.params.name.toLowerCase()];
+    if (!item) {
+      return res.status(404).json({ message: 'Item not found' });
+    }
+    res.json(item);
+  });
+
+  router.use('/items', itemRouter);
+};
