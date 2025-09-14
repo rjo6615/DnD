6476c4e2d0a1bbb0d115abee173850762c73ebf7
John Ifert-Miller <john.ifert.miller@gmail.com>
2025-09-14 19:38:49 -0400

    feat(spells): parse damage type

--- client/src/components/Zombies/attributes/PlayerTurnActions.js
+++ client/src/components/Zombies/attributes/PlayerTurnActions.js
@@ -620,7 +620,9 @@ const showSparklesEffect = () => {
                           <td>{spell.name}</td>
                           <td>{spell.casterType || spell.caster || 'Unknown'}</td>
                           <td>{spell.level}</td>
-                          <td>{formatDamageSegments(spell.damage)}</td>
+                          <td>
+                            {formatDamageSegments(`${spell.damage} ${spell.damageType ?? ''}`.trim())}
+                          </td>
                           <td>{spell.castingTime}</td>
                           <td>{spell.range}</td>
                           <td>{spell.duration}</td>
