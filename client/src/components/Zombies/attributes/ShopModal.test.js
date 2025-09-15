c56d80f5d0fbc8a92d08012304d599b3b8656ce6
Robert Obernier <robertjobernier@gmail.com>
2025-09-15 18:06:13 -0400

    Add tests for ShopModal behavior

---
+++ client/src/components/Zombies/attributes/ShopModal.test.js
@@ -0,0 +1,119 @@
+import React from 'react';
+import { render, screen, waitFor, act } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+const mockWeaponListFetch = jest.fn();
+const mockArmorListFetch = jest.fn();
+const mockItemListFetch = jest.fn();
+
+jest.mock('../../Weapons/WeaponList', () => {
+  const React = require('react');
+  const WeaponListMock = (props) => {
+    const prevShowRef = React.useRef(false);
+    React.useEffect(() => {
+      if (props.show && !prevShowRef.current) {
+        mockWeaponListFetch();
+      }
+      prevShowRef.current = props.show;
+    }, [props.show]);
+    return props.show ? <div data-testid="weapon-list">Weapons</div> : null;
+  };
+  return WeaponListMock;
+});
+
+jest.mock('../../Armor/ArmorList', () => {
+  const React = require('react');
+  const ArmorListMock = (props) => {
+    const prevShowRef = React.useRef(false);
+    React.useEffect(() => {
+      if (props.show && !prevShowRef.current) {
+        mockArmorListFetch();
+      }
+      prevShowRef.current = props.show;
+    }, [props.show]);
+    return props.show ? <div data-testid="armor-list">Armor</div> : null;
+  };
+  return ArmorListMock;
+});
+
+jest.mock('../../Items/ItemList', () => {
+  const React = require('react');
+  const ItemListMock = (props) => {
+    const prevShowRef = React.useRef(false);
+    React.useEffect(() => {
+      if (props.show && !prevShowRef.current) {
+        mockItemListFetch();
+      }
+      prevShowRef.current = props.show;
+    }, [props.show]);
+    return props.show ? <div data-testid="item-list">Items</div> : null;
+  };
+  return ItemListMock;
+});
+
+import ShopModal from './ShopModal';
+
+const defaultForm = { weapon: [], armor: [], item: [], campaign: 'alpha' };
+
+const renderShopModal = (props = {}) =>
+  render(
+    <ShopModal
+      show
+      onHide={jest.fn()}
+      onTabChange={jest.fn()}
+      onWeaponsChange={jest.fn()}
+      onArmorChange={jest.fn()}
+      onItemsChange={jest.fn()}
+      form={defaultForm}
+      characterId="char-1"
+      strength={12}
+      {...props}
+    />
+  );
+
+beforeEach(() => {
+  mockWeaponListFetch.mockClear();
+  mockArmorListFetch.mockClear();
+  mockItemListFetch.mockClear();
+});
+
+test('tab navigation switches between weapon, armor, and item views', async () => {
+  renderShopModal();
+
+  expect(screen.getByTestId('weapon-list')).toBeInTheDocument();
+  expect(screen.queryByTestId('armor-list')).not.toBeInTheDocument();
+  expect(screen.queryByTestId('item-list')).not.toBeInTheDocument();
+
+  await act(async () => {
+    await userEvent.click(screen.getByRole('tab', { name: 'Armor' }));
+  });
+  expect(await screen.findByTestId('armor-list')).toBeInTheDocument();
+  expect(screen.queryByTestId('weapon-list')).not.toBeInTheDocument();
+  expect(screen.queryByTestId('item-list')).not.toBeInTheDocument();
+
+  await act(async () => {
+    await userEvent.click(screen.getByRole('tab', { name: 'Items' }));
+  });
+  expect(await screen.findByTestId('item-list')).toBeInTheDocument();
+  expect(screen.queryByTestId('weapon-list')).not.toBeInTheDocument();
+  expect(screen.queryByTestId('armor-list')).not.toBeInTheDocument();
+});
+
+test('each list fetches once when its tab is activated', async () => {
+  renderShopModal();
+
+  await waitFor(() => expect(mockWeaponListFetch).toHaveBeenCalledTimes(1));
+  expect(mockArmorListFetch).not.toHaveBeenCalled();
+  expect(mockItemListFetch).not.toHaveBeenCalled();
+
+  await act(async () => {
+    await userEvent.click(screen.getByRole('tab', { name: 'Armor' }));
+  });
+  await waitFor(() => expect(mockArmorListFetch).toHaveBeenCalledTimes(1));
+  expect(mockItemListFetch).not.toHaveBeenCalled();
+
+  await act(async () => {
+    await userEvent.click(screen.getByRole('tab', { name: 'Items' }));
+  });
+  await waitFor(() => expect(mockItemListFetch).toHaveBeenCalledTimes(1));
+});
