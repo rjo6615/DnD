9a2db5966cc01330512e4cdd9ca1a8b8984b96b1
Robert Obernier <robertjobernier@gmail.com>
2025-09-24 18:10:35 -0400

    Handle empty combat response when passing turn

--- client/src/setupTests.js
+++ client/src/setupTests.js
@@ -3,3 +3,20 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+jest.mock(
+  'socket.io-client',
+  () => {
+    const mockSocket = {
+      on: jest.fn(),
+      off: jest.fn(),
+      emit: jest.fn(),
+      disconnect: jest.fn(),
+    };
+
+    return {
+      io: jest.fn(() => mockSocket),
+    };
+  },
+  { virtual: true }
+);
