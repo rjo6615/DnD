974e13311d32b38b166c6c93b617341bffd30d4d
John Ifert-Miller <john.ifert.miller@gmail.com>
2025-09-28 17:33:21 -0400

    Add weapon mastery metadata and tests

--- client/src/components/Weapons/WeaponDetail.js
+++ client/src/components/Weapons/WeaponDetail.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import apiFetch from '../../utils/apiFetch';
+import weaponMasteryProperties from '../../data/weaponMasteryProperties';
 
 /** @typedef {import('../../../../types/weapon').Weapon} Weapon */
 
@@ -18,11 +19,21 @@ function WeaponDetail() {
     return <div>Loading...</div>;
   }
 
+  const masteryDescription = weapon.mastery
+    ? weaponMasteryProperties[weapon.mastery] || null
+    : null;
+
   return (
     <div>
       <h2>{weapon.name}</h2>
       <p><strong>Category:</strong> {weapon.category}</p>
       <p><strong>Damage:</strong> {weapon.damage}</p>
+      {weapon.mastery && (
+        <>
+          <p><strong>Mastery:</strong> {weapon.mastery}</p>
+          {masteryDescription && <p>{masteryDescription}</p>}
+        </>
+      )}
       <p><strong>Properties:</strong> {weapon.properties.join(', ') || 'None'}</p>
       <p><strong>Weight:</strong> {weapon.weight}</p>
       <p><strong>Cost:</strong> {weapon.cost}</p>
---
+++ client/src/data/weaponMasteryProperties.js
@@ -0,0 +1,20 @@
+const weaponMasteryProperties = {
+  Cleave:
+    "If you hit a creature with a melee attack roll using this weapon, you can make a melee attack roll with the weapon against a second creature within 5 feet of the first that is also within your reach. On a hit, the second creature takes the weapon's damage, but don't add your ability modifier to that damage unless that modifier is negative. You can make this extra attack only once per turn.",
+  Graze:
+    "If your attack roll with this weapon misses a creature, you can deal damage to that creature equal to the ability modifier you used to make the attack roll. This damage is the same type dealt by the weapon, and the damage can be increased only by increasing the ability modifier.",
+  Nick:
+    "When you make the extra attack of the Light property, you can make it as part of the Attack action instead of as a Bonus Action. You can make this extra attack only once per turn.",
+  Push:
+    "If you hit a creature with this weapon, you can push the creature up to 10 feet straight away from yourself if it is Large or smaller.",
+  Sap:
+    "If you hit a creature with this weapon, that creature has Disadvantage on its next attack roll before the start of your next turn.",
+  Slow:
+    "If you hit a creature with this weapon and deal damage to it, you can reduce its Speed by 10 feet until the start of your next turn. If the creature is hit more than once by weapons that have this property, the Speed reduction doesn't exceed 10 feet.",
+  Topple:
+    "If you hit a creature with this weapon, you can force the creature to make a Constitution saving throw (DC 8 plus the ability modifier used to make the attack roll and your Proficiency Bonus). On a failed save, the creature has the Prone condition.",
+  Vex:
+    "If you hit a creature with this weapon and deal damage to the creature, you have Advantage on your next attack roll against that creature before the end of your next turn.",
+};
+
+export default weaponMasteryProperties;
