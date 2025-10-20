191848877d7dbb1d35349d7faef845c760a0ae29
rjo6615 <robertjobernier@gmail.com>
2025-10-19 20:01:43 -0400

    fixed dice damage type coloring

--- client/src/utils/damageTypeColors.js
+++ client/src/utils/damageTypeColors.js
@@ -1,12 +1,12 @@
 const damageTypeColors = {
-  acid: '#228B22',
+  acid: '#09ff09',
   cold: '#00BFFF',
   fire: '#FF4500',
-  lightning: '#FFD700',
-  poison: '#32CD32',
+  lightning: '#0026ff',
+  poison: '#c4f74e',
   thunder: '#8A2BE2',
   force: '#FF1493',
-  necrotic: '#4B0082',
+  necrotic: '#90cf80',
   radiant: '#FFFF99',
   psychic: '#BA55D3',
 };
