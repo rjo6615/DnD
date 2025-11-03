fc7dd13069bf53ea0a5f2477469c4f27c3dd9c64
Robert Obernier <robertjobernier@gmail.com>
2025-11-03 11:22:37 -0500

    updated zoom max

--- client/src/components/Zombies/attributes/MapModal.js
+++ client/src/components/Zombies/attributes/MapModal.js
@@ -35,7 +35,7 @@ const EMPTY_IDENTIFIER_SET = new Set();
 
 const BACKGROUND_ZOOM_DEFAULT = 1;
 const BACKGROUND_ZOOM_MIN = 0.5;
-const BACKGROUND_ZOOM_MAX = 4;
+const BACKGROUND_ZOOM_MAX = 6;
 
 const clampBackgroundZoom = (value) => {
   if (!Number.isFinite(value)) {
